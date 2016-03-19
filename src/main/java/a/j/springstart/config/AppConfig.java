2b52a2fcdc6fbccf2a52559302907e826c1cd68a
anjuncc <anjuncc@163.com>
2016-03-19 10:47:40 +0800

    freemarker and jsp all exist

--- src/main/java/a/j/springstart/config/AppConfig.java
+++ src/main/java/a/j/springstart/config/AppConfig.java
@@ -36,41 +36,13 @@ public class AppConfig extends  WebMvcConfigurationSupport/*WebMvcConfigurerAdap
 	    freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/ftl/");
 	    return freeMarkerConfigurer; 
 	}
-//	@Bean
-//    public ViewResolver viewResolver() {
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setViewClass(JstlView.class);
-//        viewResolver.setPrefix("/WEB-INF/views/");
-//        viewResolver.setSuffix(".jsp");
-//        return viewResolver;
-//    }
-//    @Bean
-//    public FreeMarkerViewResolver freeMarkerViewResolver() {
-//        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
-//        viewResolver.setPrefix("/WEB-INF/ftl/");
-//        viewResolver.setSuffix(".ftl");
-//        viewResolver.setCache(true);
-//        viewResolver.setContentType("text/html;charset=UTF-8");
-//        viewResolver.setRequestContextAttribute("requestContext");
-//        viewResolver.setOrder(0);
-//        return viewResolver;
-//    }
-//    @Bean
-//    public FreeMarkerConfig freeMarkerConfig(){
-//        FreeMarkerConfigurer fmCfg=new FreeMarkerConfigurer();
-//        freemarker.template.Configuration tplCfg = new freemarker.template.Configuration();
-//        tplCfg.setTagSyntax(freemarker.template.Configuration.AUTO_DETECT_TAG_SYNTAX);
-//        tplCfg.setTemplateUpdateDelay(1);
-//        tplCfg.setDefaultEncoding("UTF-8");
-//        tplCfg.setOutputEncoding("UTF-8");
-//        tplCfg.setLocale(Locale.SIMPLIFIED_CHINESE);
-//          tplCfg.setDateFormat("yyyy-MM-dd");
-//        tplCfg.setTimeFormat("HH:mm:ss");
-//        tplCfg.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
-//        tplCfg.setClassicCompatible(true);//空串显示""
-//        tplCfg.setTemplateLoader(new WebappTemplateLoader(getServletContext()));
-//        fmCfg.setConfiguration(tplCfg);
-//    
-//        return fmCfg;
-//    }
+	@Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
 }
