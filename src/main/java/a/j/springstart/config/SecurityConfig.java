04c1d961239e61f763853478328ff7807517cfb7
anjuncc <anjuncc@163.com>
2016-03-19 18:58:40 +0800

    spring securit method @PreAuthorize,@PostAuthorize, @Secured, EL

--- src/main/java/a/j/springstart/config/SecurityConfig.java
+++ src/main/java/a/j/springstart/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,6 +30,7 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
--- src/main/java/a/j/springstart/service/UserService.java
+++ src/main/java/a/j/springstart/service/UserService.java
@@ -3,6 +3,9 @@
  */
 package a.j.springstart.service;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import a.j.springstart.domain.User;
 
 /**
@@ -19,5 +22,23 @@ public interface UserService {
 	 * @param user
 	 */
 	void save(User user);
+	
+//    List<User> findAllUsers();
+//    
+//    @Secured("ROLE_ADMIN")
+//    void updateUser(User user);
+// 
+//    @Secured({ "ROLE_DBA", "ROLE_ADMIN" })
+//    void deleteUser();
+    
+//    @PostAuthorize ("returnObject.type == authentication.name")
+//    User findById(int id);
+// 
+//    @PreAuthorize("hasRole('ADMIN')")
+//    void updateUser(User user);
+//     
+//    @PreAuthorize("hasRole('ADMIN') AND hasRole('DBA')")
+//    void deleteUser(int id);
+    
      
 }
