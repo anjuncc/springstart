8155692d9a2f7232a761f5aeb5703d79f4d095bb
anjuncc <anjuncc@163.com>
2016-03-19 10:39:38 +0800

    add freemark

--- src/main/java/a/j/springstart/config/AppConfig.java
+++ src/main/java/a/j/springstart/config/AppConfig.java
@@ -1,26 +1,76 @@
 package a.j.springstart.config;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+import freemarker.cache.WebappTemplateLoader;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "a.j.springstart.controller" })
 @ImportResource({ "classpath:beans-config.xml" })
-public class AppConfig extends WebMvcConfigurerAdapter {
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
+public class AppConfig extends  WebMvcConfigurationSupport/*WebMvcConfigurerAdapter*/ {
+	@Bean 
+	public FreeMarkerViewResolver freemarkerViewResolver() { 
+	    FreeMarkerViewResolver resolver = new FreeMarkerViewResolver(); 
+	    resolver.setCache(true); 
+	    resolver.setPrefix(""); 
+	    resolver.setSuffix(".ftl"); 
+	    return resolver; 
+	}
+	@Bean 
+	public FreeMarkerConfigurer freemarkerConfig() { 
+	    FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer(); 
+	    freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views/ftl/");
+	    return freeMarkerConfigurer; 
+	}
+//	@Bean
+//    public ViewResolver viewResolver() {
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setViewClass(JstlView.class);
+//        viewResolver.setPrefix("/WEB-INF/views/");
+//        viewResolver.setSuffix(".jsp");
+//        return viewResolver;
+//    }
+//    @Bean
+//    public FreeMarkerViewResolver freeMarkerViewResolver() {
+//        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/ftl/");
+//        viewResolver.setSuffix(".ftl");
+//        viewResolver.setCache(true);
+//        viewResolver.setContentType("text/html;charset=UTF-8");
+//        viewResolver.setRequestContextAttribute("requestContext");
+//        viewResolver.setOrder(0);
+//        return viewResolver;
+//    }
+//    @Bean
+//    public FreeMarkerConfig freeMarkerConfig(){
+//        FreeMarkerConfigurer fmCfg=new FreeMarkerConfigurer();
+//        freemarker.template.Configuration tplCfg = new freemarker.template.Configuration();
+//        tplCfg.setTagSyntax(freemarker.template.Configuration.AUTO_DETECT_TAG_SYNTAX);
+//        tplCfg.setTemplateUpdateDelay(1);
+//        tplCfg.setDefaultEncoding("UTF-8");
+//        tplCfg.setOutputEncoding("UTF-8");
+//        tplCfg.setLocale(Locale.SIMPLIFIED_CHINESE);
+//          tplCfg.setDateFormat("yyyy-MM-dd");
+//        tplCfg.setTimeFormat("HH:mm:ss");
+//        tplCfg.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
+//        tplCfg.setClassicCompatible(true);//空串显示""
+//        tplCfg.setTemplateLoader(new WebappTemplateLoader(getServletContext()));
+//        fmCfg.setConfiguration(tplCfg);
+//    
+//        return fmCfg;
+//    }
 }
--- src/main/java/a/j/springstart/controller/Index.java
+++ src/main/java/a/j/springstart/controller/Index.java
@@ -3,8 +3,12 @@
  */
 package a.j.springstart.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,9 +19,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/index")
 public class Index {
-	 @RequestMapping(value = "/index", method = RequestMethod.GET)
-	    public String sayHelloAgain(ModelMap model) {
-	        model.addAttribute("greeting", "this a index/index");
-	        return "welcome";
-	    }
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String sayHelloAgain(ModelMap model) {
+		model.addAttribute("greeting", "this a index/index");
+		return "welcome";
+	}
+
+	@RequestMapping(value = "/cars", method = RequestMethod.GET)
+	public String init(@ModelAttribute("model") ModelMap model) {
+		List<String>carList =new ArrayList<>();
+		carList.add("test");
+		carList.add("test2");
+		carList.add("test3");
+		model.addAttribute("carList", carList);
+		return "index";
+	}
 }
